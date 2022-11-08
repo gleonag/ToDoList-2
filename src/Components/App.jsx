35ff1bc45a9a0e55e7b58101af5a9b626f5afe44
gleonag <gleonag@gmail.com>
2022-11-08 17:02:17 +0000

    doing components

---
+++ src/Components/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import TodoForm from "./TodoForm";
+
+
+const App = () => {
+	return (
+		<div className="todo-app">
+			<TodoForm />
+		</div>
+	);
+};
+
+export default App;
---
+++ src/Components/TodoList.js
@@ -0,0 +1,15 @@
+import React, {useState} from 'react'
+import TodoForm from './TodoForm'
+
+function TodoList() {
+
+    const [todos, setTodos] = useState([])
+
+  return (
+    <div>
+        <TodoForm/>
+    </div>
+  )
+}
+
+export default TodoList
